0e0e7968a8e20b765aeaada215cd318eb002009b
Malvais <al221910499@gmail.com>
2024-03-26 23:25:13 -0600

    Cambios en el Login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts/blankLayout')
+
+@section('title', 'Login Basic - Pages')
+
+@section('page-style')
+<link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/page-auth.css') }}">
+@endsection
+
+@section('content')
+<div class="container-xxl">
+    <div class="authentication-wrapper authentication-basic container-p-y">
+        <div class="authentication-inner">
+            <div class="card">
+                <div class="card-body">
+                    <div class="app-brand justify-content-center">
+                        <span class="app-brand-logo demo">
+                            <img src="assets\img\icons\brands\reci.png" alt="Descripción de la imagen">
+                        </span>
+                        </a>
+                    </div>
+                    <h4 class="mb-2">Bienvenido a ReciclaguaS</h4>
+                    <p class="mb-4">Por favor, inicie sesión:</p>
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="username" class="form-label">Usuario</label>
+                            <input type="text" class="form-control @error('username') is-invalid @enderror" id="username" name="username" placeholder="Ingresa tu usuario" autofocus>
+
+                        </div>
+                        <div class="mb-3 form-password-toggle">
+                            <div class="d-flex justify-content-between">
+                                <label class="form-label" for="password">Contraseña</label>
+                                <a href="{{ url('auth/forgot-password-basic') }}">
+                            </div>
+                            <div class="input-group input-group-merge">
+                                <input type="password" id="password" class="form-control" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
+                                <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                            </div>
+                        </div>
+
+                        <div class="mb-3">
+                            <button class="btn btn-primary d-grid w-100" type="submit">Iniciar Sesión</button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+            </div>
+    </div>
+</div>
+</div>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+@if (session('swal'))
+    <script>
+        Swal.fire({
+            title: '{{ session('swal')['title'] }}',
+            text: '{{ session('swal')['text'] }}',
+            icon: '{{ session('swal')['type'] }}',
+            confirmButtonText: 'Aceptar',
+            confirmButtonColor: '{{ session('swal')['confirmButtonColor'] }}',
+            allowOutsideClick: false,
+        });
+    </script>
+@endif
+@endsection
